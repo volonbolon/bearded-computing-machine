80184934da4384e677f99ecee3bc95f3902f7ba8
Ariel Rodriguez <ariel@volonbolon.net>
2014-09-23 20:34:29 -0300

    Add Xcode Project

---
+++ BeardedComputingMachine/BeardedComputingMachine/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  BeardedComputingMachine
+//
+//  Created by Ariel Rodriguez on 9/23/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ BeardedComputingMachine/BeardedComputingMachine/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  BeardedComputingMachine
+//
+//  Created by Ariel Rodriguez on 9/23/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
